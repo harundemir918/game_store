aed7b263b19c92b4aa4726b7614fb655cbe72f23
Harun Demir <59452532+harundemir918@users.noreply.github.com>
2023-09-08 20:19:35 +0300

    category horizontal items navigate to detail added

--- app/src/main/java/org/harundemir/gamestore/adapters/CategoriesHorizontalAdapter.kt
+++ app/src/main/java/org/harundemir/gamestore/adapters/CategoriesHorizontalAdapter.kt
@@ -1,24 +1,34 @@
 package org.harundemir.gamestore.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.harundemir.gamestore.R
+import org.harundemir.gamestore.activities.CategoryDetailActivity
 import org.harundemir.gamestore.models.Category
 
 class CategoriesHorizontalAdapter(private val categories: List<Category>) :
     RecyclerView.Adapter<CategoriesHorizontalAdapter.CategoryViewHolder>() {
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(category: Category) {
+            val categoryHorizontalItem = itemView.findViewById<LinearLayout>(R.id.categoryHorizontalItem)
             val categoryImage =
                 itemView.findViewById<ImageView>(R.id.category_horizontal_item_image)
             val categoryTitle = itemView.findViewById<TextView>(R.id.category_horizontal_item_title)
 
             categoryImage.setImageResource(category.icon)
             categoryTitle.text = category.title
+
+            categoryHorizontalItem.setOnClickListener {
+                val intent = Intent(itemView.context, CategoryDetailActivity::class.java)
+                intent.putExtra("category", category)
+                itemView.context.startActivity(intent)
+            }
         }
     }
 
